4b15449dd35e83955f35d79ddc25d1b2412146b8
laragim23 <109818935+laragim23@users.noreply.github.com>
2023-12-04 10:08:28 -0300

    apaguei tudo

--- conexao.php
+++
@@ -1,4 +0,0 @@
-<?php 
-//1. Conectar no BD (IP, usuario, senha, nome do bd)
-$conexao = mysqli_connect('127.0.0.1', 'root', '', 'tcc');
-?>
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
-define('BASE', 'tcc');
-
-$conn = new MySQLI(HOST, USER, PASS, BASE);
-?>
